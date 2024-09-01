81663d5b3efcd052fbcbc107af7e4b8caaffed51
insub <vb15@kakao.com>
2024-09-01 15:10:10 +0900

    Removed Example Folder

--- Package.swift
+++ Package.swift
@@ -16,11 +16,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CalendarKit",
-            path: "Sources/CalendarKit"),
-        .target(
-            name: "Example",
-            dependencies: ["CalendarKit"],
-            path: "Example")
+            name: "CalendarKit"),
     ]
 )
